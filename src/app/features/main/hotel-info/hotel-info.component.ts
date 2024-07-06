b7d760ce18ed68b900a73c9d82ac71f68a5312f0
Jeniffer Balabuch <balabuchj@gmail.com>
2024-07-06 01:27:10 -0400

    FEAT: Hotel Info

---
+++ src/app/features/main/hotel-info/hotel-info.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hotel-info',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './hotel-info.component.html',
+  styleUrl: './hotel-info.component.scss'
+})
+export class HotelInfoComponent {
+
+}
--- src/app/features/main/main.component.ts
+++ src/app/features/main/main.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { GalleryComponent } from '../../shared/gallery/gallery.component';
 import { RibbonComponent } from '../../shared/ribbon/ribbon.component';
+import { HotelInfoComponent } from './hotel-info/hotel-info.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule, GalleryComponent, RibbonComponent],
+  imports: [CommonModule, GalleryComponent, RibbonComponent, HotelInfoComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
