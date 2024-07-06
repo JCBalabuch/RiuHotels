3139f752f6190affbe3e2eb7e0973439cfd8c4e1
Jeniffer Balabuch <balabuchj@gmail.com>
2024-07-06 18:38:42 -0400

    FEAT: footer-rrss

--- src/app/core/footer/footer-rrss/footer-rrss.component.ts
+++ src/app/core/footer/footer-rrss/footer-rrss.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer-rrss',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer-rrss.component.html',
   styleUrl: './footer-rrss.component.scss'
 })
