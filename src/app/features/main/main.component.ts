46cd57f4801c0c06305424252b1fb044e87386ff
Jeniffer Balabuch <balabuchj@gmail.com>
2024-07-06 00:37:38 -0400

    FEAT: models & data

--- src/app/features/main/main.component.ts
+++ src/app/features/main/main.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { GalleryComponent } from '../../shared/gallery/gallery.component';
+import { RibbonComponent } from '../../shared/ribbon/ribbon.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule, GalleryComponent],
+  imports: [CommonModule, GalleryComponent, RibbonComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
